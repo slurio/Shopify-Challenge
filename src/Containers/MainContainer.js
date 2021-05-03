04e9df391a4443341c0ee9cb1574db290a8ea856
slurio <samantha.lurio@gmail.com>
2021-05-03 17:01:38 -0400

    modal nomination limit done

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -56,7 +56,6 @@ class Main extends React.Component {
     // adds movie to nominations and updates its state
     addNominationClickHandler = (movie) => {
             let updatedNominations = [...this.state.nominations];
-            let modal = document.querySelector('.test');
 
             if (this.state.nominations.length !== 5) {
                 updatedNominations.push(movie)
@@ -65,6 +64,8 @@ class Main extends React.Component {
                 })
             } else if (this.state.nominations.length === 5) {
                 this.setState({ nominationLimit: true });
+                let modal = document.querySelector('.test');
+                modal.style.display = 'flex';
             }
     }
 
@@ -79,15 +80,22 @@ class Main extends React.Component {
         })
     }
 
-    closeModal = (modal) => {
+    closeModal = () => {
         this.setState({ nominationLimit: true });
-        modal.style.height = '0px';
+        let modal = document.querySelector('.test')
+        modal.style.display = 'none';
     }
 
     render() {
         return(
             <>
-            {this.state.nominationLimit ? <NominationLimitModal closeModal={this.closeModal}/> : null}
+            {this.state.nominationLimit ?  <Modal className='test'>
+            <h3>You have no more nominations left.</h3>
+            <button onClick={this.closeModal}>ok</button>
+        </Modal> : <Modal className='test' style={{display: 'none'}}>
+            <h3>You have no more nominations left.</h3>
+            <button onClick={this.closeModal}>ok</button>
+        </Modal>}
                 {this.state.nominations.length === 5 ?
                 <Banner>
                     <Envelope src={envelope}/>
@@ -200,4 +208,19 @@ const DataContainer = styled.div`
     flex-direction: row;
     justify-content: space-between;
     margin: 20px 0px;
+`
+
+const Modal = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    width: 450px;
+    height: auto;
 `
